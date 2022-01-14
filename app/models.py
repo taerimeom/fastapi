b0bd8eb145c3b1621d47ffd448ee7655b7e4df16
Taerim Eom <taeirm.eom.official@gmail.com>
2022-01-13 23:24:33 -0500

    timestamp

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import text, true
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 
@@ -8,4 +10,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="TRUE", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=true), nullable=False, server_default=text("now()")
+    )
